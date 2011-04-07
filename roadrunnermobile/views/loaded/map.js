7fa6dac805dd143a59ec9c78bd2dc47357a237e8
Wolfi <halbeisen@hotmail.com>
2011-04-07 11:42:07 +0200

    added reduce for loaded items

--- roadrunnermobile/views/loaded/map.js
+++ roadrunnermobile/views/loaded/map.js
@@ -1,5 +1,7 @@
 function(doc) {
-	if (doc.type && doc.type == 'log')  {
-		emit([doc.item, doc.logType], doc);
+	if (doc.type && doc.logType && doc.type == 'log')  {
+		if (doc.logType == 'LOAD' || doc.logType == 'UNLOAD')  {
+			emit([doc.item, doc.timestamp, doc.logType], null);
+		}
 	}
 }
---
+++ roadrunnermobile/views/loaded/reduce.js
@@ -0,0 +1,27 @@
+function(keys, values) {
+	
+	var key;
+	var item;
+	var timestamp;
+	var logType;
+	
+	var lastItem;
+	var loadedItems = [];
+
+	for(var i in keys) {
+		
+		key = keys[i];
+		item = key[0][0];
+		timestamp = key[0][1];
+		logType = key[0][2];
+		
+		if (!lastItem || lastItem != item)  {
+			lastItem = item;
+			if (logType == 'LOAD')  {
+				loadedItems.push([item, timestamp]);
+			}
+		}
+	}
+
+	return loadedItems;
+}
