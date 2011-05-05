fb4f5b3e4172970e672f1da0d934d8c79b329a59
Wolfi <halbeisen@hotmail.com>
2011-05-05 09:46:21 +0200

    improved validation for documents

--- roadrunnerschema/validate_doc_update.js
+++ roadrunnerschema/validate_doc_update.js
@@ -14,10 +14,12 @@ function (newDoc, oldDoc, userCtx ) {
 		throw({forbidden : 'unknown document'});
 	}
 	
+	// for CouchDB
 	docSchema.properties._id = {type : 'string', required : true};
-	docSchema.properties._rev = {type : 'string', required : true};
+	docSchema.properties._rev = {type : 'string', required : false};
 	docSchema.properties._revisions = {type : 'object', required : false};
-	
+	// for Doctrine
+	docSchema.properties.doctrine_metadata = {type : 'object', required : false};
 	
 	var res = Validator.validate(newDoc, docSchema);
 
