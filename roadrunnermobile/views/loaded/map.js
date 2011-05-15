246623ca108349713d2926068fcaed0f9cc39a31
Wolfi <halbeisen@hotmail.com>
2011-05-15 17:20:01 +0200

    loaded-view now working with log-schema

--- roadrunnermobile/views/loaded/map.js
+++ roadrunnermobile/views/loaded/map.js
@@ -1,7 +1,9 @@
 function(doc) {
 	if (doc.type && doc.logType && doc.type == 'log')  {
 		if (doc.logType == 'LOAD' || doc.logType == 'UNLOAD')  {
-			emit(doc.item, [doc.logType, doc.timestamp]);
+			for (var i in doc.items)  {
+				emit(doc.items[i], [doc.logType, doc.timestamp]);
+			}
 		}
 	}
 }
