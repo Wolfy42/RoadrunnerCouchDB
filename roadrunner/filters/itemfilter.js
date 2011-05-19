98d7ba3d5cf958894eadea97805994a3b542e86d
Wolfi <halbeisen@hotmail.com>
2011-05-19 10:11:00 +0200

    extended itemFilter for Items and Deliverys

--- roadrunner/filters/itemfilter.js
+++ roadrunner/filters/itemfilter.js
@@ -1,13 +1,19 @@
 function(doc, req) {
+	if (!req.query || !req.query.items || !doc.type)  {
+		return false;
+	}
+	var items = req.query.items;
 	
-	if (doc.type && doc.type == "item" && req.query && req.query.items) {
-		
-		var items = req.query.items;
-		
+	if (doc.type == "item") {
 		if (items.indexOf(doc._id) != -1)  {
 			return true;
 		}
+	}  else if (doc.type == "delivery")  {
+		for (var i in doc.items)  {
+			if (items.indexOf(doc.items[i]) != -1)  {
+				return true
+			}
+		}
 	}
-	
 	return false;
 }
