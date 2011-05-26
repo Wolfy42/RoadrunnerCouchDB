3a1213504d13474a20bb9973fe8feb8cb0eca83d
Wolfi <halbeisen@hotmail.com>
2011-05-26 10:41:15 +0200

    added install-scripts for Design Documents

---
+++ install_designdocuments.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+couchapp push roadrunner http://roadrunner:roadrunner@roadrunner.server:5984/roadrunner
+couchapp push roadrunnermobile http://roadrunner:roadrunner@roadrunner.server:5984/roadrunner
+couchapp push roadrunnerschema http://roadrunner:roadrunner@roadrunner.server:5984/roadrunner
