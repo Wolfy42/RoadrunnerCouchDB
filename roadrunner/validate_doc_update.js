366392f0cd5bb89f6a0e19d90b429065756d482e
Wolfi <halbeisen@hotmail.com>
2011-05-26 12:54:58 +0200

    Admin is now allowed to do anything. Just people from office can delete documents.

--- roadrunner/validate_doc_update.js
+++ roadrunner/validate_doc_update.js
@@ -1,10 +1,24 @@
 function (newDoc, oldDoc, userCtx ) {
 	
+	var admin = 'admin';
+	var office = 'office';
+	var driver = 'driver';
+	
+	//Admin is allowed to do anything
+	if (userCtx.roles.indexOf(admin) != -1)  {
+		return;
+	}
+	
 	if (newDoc.type == 'log' && oldDoc != null)  {
 		throw({forbidden : 'Logs are unchangeable!'});
 	}
 	if (oldDoc && oldDoc.type == 'log' && newDoc._deleted == true)  {
 		throw({forbidden : 'Logs cannot be deleted!'});
+	}
+	if (newDoc._deleted && newDoc._deleted == true)  {
+		if (userCtx.roles.indexOf(office) == -1)  {
+			throw({forbidden : 'Insufficient rights!'});
+		}
 	}
 }
 
