9436f033a02e038957d2ee062e5c245ced45acd2
Wolfi <halbeisen@hotmail.com>
2011-05-27 13:16:25 +0200

    changed loaded-view from mobile-design-document: type UNREGISTER is for the view like UNLOAD

--- roadrunnermobile/views/loaded/map.js
+++ roadrunnermobile/views/loaded/map.js
@@ -1,8 +1,15 @@
 function(doc) {
+	var logType;
 	if (doc.type && doc.logType && doc.type == 'log')  {
-		if (doc.logType == 'LOAD' || doc.logType == 'UNLOAD')  {
+		if (doc.logType == 'LOAD' || doc.logType == 'UNLOAD' || doc.logType == 'UNREGISTER')  {
+			
+			if (doc.logType == 'LOAD')  {
+				logType = 'LOAD';
+			}  else  {
+				logType = 'UNLOAD';
+			}
 			for (var i in doc.items)  {
-				emit(doc.items[i], [doc.logType, doc.timestamp]);
+				emit(doc.items[i], [logType, doc.timestamp]);
 			}
 		}
 	}
