924ad9c6add949da4b157707fd86108235e76d7e
Wolfi <halbeisen@hotmail.com>
2011-06-22 15:04:20 +0200

    changed view for deliveryforitem to return just the doc-id

--- roadrunner/views/deliveryforitem/map.js
+++ roadrunner/views/deliveryforitem/map.js
@@ -2,7 +2,7 @@ function(doc) {
 	if (doc.type && doc.type == "delivery")  {
 		var items = doc.doctrine_metadata.associations.items;
 		for (var i in items)  {
-			emit(items[i], doc);
+			emit(items[i], doc._id);
 		}
 	}
 }
